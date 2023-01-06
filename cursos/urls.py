fb4403bd611da6783dcf2a1d13676e32e06a303b
israelnp <israelpereira1984@gmail.com>
2023-01-06 16:08:13 -0300

    overwrite generics methods

--- cursos/urls.py
+++ cursos/urls.py
@@ -6,7 +6,12 @@ from .views import (AvaliacaoAPIView, AvaliacoesAPIView, CursoAPIView,
 urlpatterns = [
     path('cursos/', CursosAPIView.as_view(), name='cursos'),
     path('cursos/<int:pk>/', CursoAPIView.as_view(), name='curso'),
+    path('cursos/<int:curso_pk>/avaliacoes/',
+         AvaliacoesAPIView.as_view(), name='curso_avaliacoes'),
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>/',
+         AvaliacaoAPIView.as_view(), name='curso_avaliacao'),
+
     path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
-    path('avaliacoes/<int:pk>/',
+    path('avaliacoes/<int:avaliacao_pk>/',
          AvaliacaoAPIView.as_view(), name='avaliacao'),
 ]
