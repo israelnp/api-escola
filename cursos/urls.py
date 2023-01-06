52148776594afd5d63ca38ef530857cda1896a50
israelnp <israelpereira1984@gmail.com>
2023-01-06 17:22:45 -0300

    add  viewsets and routers

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from .views import (AvaliacaoAPIView, AvaliacoesAPIView, CursoAPIView,
-                    CursosAPIView)
+from .views import (
+    CursoAPIView,
+    CursosAPIView,
+    AvaliacaoAPIView,
+    AvaliacoesAPIView,
+    CursoViewSet,
+    AvaliacaoViewSet)
+
+
+router = SimpleRouter()
+router.register('cursos', CursoViewSet)
+router.register('avaliacoes', AvaliacaoViewSet)
+
 
 urlpatterns = [
     path('cursos/', CursosAPIView.as_view(), name='cursos'),
--- cursos/views.py
+++ cursos/views.py
@@ -1,5 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
+from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
 
 from .models import Avaliacao, Curso
 from .serializers import AvaliacaoSerializer, CursoSerializer
@@ -39,3 +41,24 @@ class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
                                      curso_id=self.kwargs.get('curso_pk'),
                                      pk=self.kwargs.get('avaliacao_pk'))
         return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
+
+
+"""
+API V2
+"""
+
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+    @action(detail=True, methods=['get'])
+    def avaliacoes(self, request, pk=None):
+        curso = self.get_object()
+        serializer = AvaliacaoSerializer(curso.avaliacoes.all(), many=True)
+        return Response(serializer.data)
+
+
+class AvaliacaoViewSet(viewsets.ModelViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
